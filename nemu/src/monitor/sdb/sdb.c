a447d13b8efe71098c9a5d080d85ed56fa0939b2
tracer-ysyx <tracer@ysyx.org>
2025-01-16 10:53:45 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:53:45 up  1:55,  2 users,  load average: 0.05, 0.10, 0.15

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -73,6 +73,8 @@ static int cmd_si(char *args){
 static int cmd_info(char *args){
 	if(strcmp(args,"r")==0)
 	 isa_reg_display();
+	if(args == NULL)
+		printf("Please input info r or info w");
 	return 0;
 }
 
