d9ecc867fe8a3f165446967fd96bf68a73799248
tracer-ysyx <tracer@ysyx.org>
2025-01-17 15:41:58 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:41:58 up  6:41,  2 users,  load average: 0.00, 0.01, 0.05

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -193,20 +193,18 @@ static bool make_token(char *e) {
 			Tokens_OP[n] = Tokens_DD[x];	
 			x++;
 			s++;
-			printf("666");
 		}
 		if(lenth2 != 0 && x == lenth1 && y < lenth2 ){
 			Tokens_OP[n] = Tokens_PM[y];
 			y++;
 			s++;
 			printf("%d\n",Tokens_OP[n]);
-			printf("777");
 		}
 		else
 			break;
 	}
 	int OP;
-	OP = Tokens_OP[s-1];	
+	OP = Tokens_OP[s];	
 	return OP;	
 	}
 
