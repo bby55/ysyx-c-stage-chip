7911cfa6283b0e0a3cbf2e80d54005263f0958a4
tracer-ysyx <tracer@ysyx.org>
2025-01-18 11:36:12 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:36:12 up  2:40,  2 users,  load average: 0.03, 0.01, 0.02

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -299,7 +299,9 @@ word_t eval(int p, int q) {
 			}
 			else if(tokens[p].type == TK_REG){
 				bool success = true;
-				return isa_reg_str2val(tokens[p].str,&success);
+				static char hex_str[20];
+				sprintf(hex_str, "0x%x",isa_reg_str2val(tokens[p].str,&success));
+				return strtoul(hex_str, NULL, 16);
 			}
 			else{
 				printf("格式错误，程序退出");
