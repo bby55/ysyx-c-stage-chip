a8753c73e3cb546b73e72ab9529979c3132ac607
tracer-ysyx <tracer@ysyx.org>
2025-01-19 21:17:42 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:17:42 up 12:20,  2 users,  load average: 0.12, 0.27, 0.33

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -100,8 +100,8 @@ static bool make_token(char *e) {
 
 			if(nr_token < 3200){
         switch (rules[i].token_type) {
-					//case TK_NOTYPE:
-							//break;
+					case TK_NOTYPE:
+							break;
 					case TK_EQ:
 							tokens[nr_token].type =	TK_EQ; 
 							strncpy(tokens[nr_token].str, "==", sizeof(tokens[nr_token].str));
@@ -353,7 +353,6 @@ word_t eval(int p, int q) {
     /* The expression is surrounded by a matched pair of parentheses.
      * If that is the case, just throw away the parentheses.
      */
-			printf("TRUE");
 			return eval(p + 1, q - 1);
 		}
 		else {
