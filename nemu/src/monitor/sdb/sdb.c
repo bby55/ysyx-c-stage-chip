ada8360bd52acb111f65ebd3dbadbebba2b7613e
tracer-ysyx <tracer@ysyx.org>
2025-01-19 16:11:34 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:11:34 up  7:13,  2 users,  load average: 0.08, 0.06, 0.12

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -248,8 +248,9 @@ void test_expr() {
     }
 
     // 打印调试信息
-		char *rubbish = strchr(e, '@');
-		char *expression = rubbish + 2; 
+		char *rubbish = strtok(e,"@");
+		char *expression = strtok(NULL,"@"); 
+		(void)rubbish;
     printf("expression: %s\n", expression);
     // 调用 expr() 函数求值
     word_t res = expr(expression, &success);
