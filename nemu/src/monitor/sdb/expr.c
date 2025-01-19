cc4008df76b9672671b2499b1310845486056811
tracer-ysyx <tracer@ysyx.org>
2025-01-19 15:03:32 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:03:32 up  6:05,  2 users,  load average: 0.09, 0.07, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -110,6 +110,8 @@ static bool make_token(char *e) {
          */
 			if(nr_token < 320){
         switch (rules[i].token_type) {
+					case TK_NOTYPE:
+							break;
 					case TK_EQ:
 							tokens[nr_token].type =	TK_EQ; 
 							strncpy(tokens[nr_token].str, "==", sizeof(tokens[nr_token].str));
