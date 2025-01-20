0306229983dfbb4c423f22bbdb4e093686a3a191
tracer-ysyx <tracer@ysyx.org>
2025-01-20 16:14:34 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:14:34 up  7:19,  2 users,  load average: 0.07, 0.03, 0.08

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -121,7 +121,7 @@ void check_watchpoint(){
 	for(i = 0;i < NR_WP;i++){
 		if(wp_pool[i].old_val != wp_pool[i].new_val){
 			nemu_state.state = NEMU_STOP;
-			printf("触发监视点，程序暂停");
+			printf("触发监视点，程序暂停\n");
 	}
 }
 }
