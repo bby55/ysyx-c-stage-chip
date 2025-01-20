ab0302dae72109e4cfb555ec749f6530073efec0
tracer-ysyx <tracer@ysyx.org>
2025-01-20 17:01:23 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:01:23 up  8:06,  2 users,  load average: 0.10, 0.06, 0.08

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -29,8 +29,9 @@ static bool g_print_step = false;
 /*void display_watchpoint();
 void update_watchpoint();
 void check_watchpoint();
-void device_update();
 */
+void device_update();
+
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
