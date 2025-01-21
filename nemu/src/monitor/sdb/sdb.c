4c89d1ff1edfcaea6fcf14e9d2a5ba0e0cd42a63
tracer-ysyx <tracer@ysyx.org>
2025-01-21 11:43:59 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:43:59 up  2:40,  2 users,  load average: 0.21, 0.11, 0.15

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -268,9 +268,9 @@ void init_sdb() {
   /* Compile the regular expressions. */
   init_regex();
   /* test math expression calculation */
-//test_expr();
-//printf("测试用例数:%d\n",pass_count);
-//printf("通过测试数:%d\n",pass_count);
+test_expr();
+printf("测试用例数:%d\n",pass_count);
+printf("通过测试数:%d\n",pass_count);
   /* Initialize the watchpoint pool. */
   init_wp_pool();
 }
