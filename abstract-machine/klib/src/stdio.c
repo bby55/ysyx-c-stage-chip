17a374ccb06e5451ba28c1f0b0aeea70c391ddea
tracer-ysyx <tracer@ysyx.org>
2025-07-22 16:32:06 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:32:06 up  7:33,  2 users,  load average: 0.74, 0.26, 0.20

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -51,44 +51,42 @@ static int int_to_str(int num, char *buf) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
+
     va_list args;
     va_start(args, fmt);
-
-    int j = 0; 
-    char int_buf[20]; 
-    char *p;
-
-    for (size_t i = 0; fmt[i] != '\0'; i++) {
-        if (fmt[i] == '%') {
-            i++; 
-            switch (fmt[i]) {
-                case 'd': {
-                    int len = int_to_str(va_arg(args, int), int_buf);
-                    for (int k = 0; k < len; k++) {
-                        out[j++] = int_buf[k];
+    int j = 0;
+    int len = 0;
+    char int_buf[20];
+    char *p = out;
+    for(size_t i = 0; i < strlen(fmt); i++){
+        if(fmt[i] == '%'){
+            switch(fmt[i+1]){
+                case 'd':
+                    len = int_to_str(va_arg(args, int), int_buf);
+                    for(int k = 0; k < len; k++){
+                        out[j] = int_buf[k];
+                        j++;
                     }
+                    i++;
                     break;
-                }
-                case 's': {
+
+                case 's':
                     p = va_arg(args, char*);
-                    if (p != NULL) {
-                        for (int k = 0; p[k] != '\0'; k++) {
-                            out[j++] = p[k];
-                        }
+                    for(int k = 0; k < strlen(p); k++){
+                        out[j] = p[k];
+                        j++;
                     }
+                    i++;
                     break;
-                }
-                default: {
-                    out[j++] = '%';   
-                    out[j++] = fmt[i];
+                default:
+                    out[j++] = '%';
+                    out[j] = fmt[i+1];
+                    j++;
+                    i++;
                     break;
-                }
             }
-        } else {
-            out[j++] = fmt[i];
-        }
     }
-
+    }
     out[j] = '\0';
     va_end(args);
     return j;
