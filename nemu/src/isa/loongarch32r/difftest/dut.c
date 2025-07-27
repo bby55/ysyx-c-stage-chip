a09b8df05d9bd3b5d347f50132f2e52c103d130c
tracer-ysyx <tracer@ysyx.org>
2025-07-28 00:06:50 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:06:50 up 14:57,  2 users,  load average: 1.03, 0.59, 0.43

--- nemu/src/isa/loongarch32r/difftest/dut.c
+++ nemu/src/isa/loongarch32r/difftest/dut.c
@@ -18,7 +18,7 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  int num = ARRLEN(cpu.gpr);
+  /*int num = ARRLEN(cpu.gpr);
   for(int i =0; i < num; i++){
     if(ref_r->gpr[i] != cpu->gpr[i]){
       return false;
@@ -27,7 +27,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
       return false;
     }
   }
-  return true;
+  return true;*/
 }
 
 void isa_difftest_attach() {
