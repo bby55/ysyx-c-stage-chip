df81237e8ca5769cb287c6586d877076679386b9
tracer-ysyx <tracer@ysyx.org>
2025-07-28 00:03:29 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:03:29 up 14:53,  2 users,  load average: 0.43, 0.48, 0.38

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -86,8 +86,8 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
     assert(0);
   }
 }
-ifftest_regcpy
-__EXPORT void d(void* dut, bool direction) {
+
+__EXPORT void difftest_regcpy(void* dut, bool direction) {
   if (direction == DIFFTEST_TO_REF) {
     s->diff_set_regs(dut);
   } else {
