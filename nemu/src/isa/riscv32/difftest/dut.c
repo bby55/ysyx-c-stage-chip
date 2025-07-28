b6bc7b8601d59885e87b77f6b34d0f820b70c337
tracer-ysyx <tracer@ysyx.org>
2025-07-28 11:27:00 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:27:00 up  2:14,  2 users,  load average: 0.34, 0.44, 0.72

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -32,11 +32,11 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   }
   if (ref_r->pc != cpu.pc) {
     printf("\n");
-    printf("\033[1;31m-------!Difftest!-------\n\033[0m");
-    printf("\033[1;31m////////////////////////\n\033[0m");   
-    printf("\033[1;31m//\033[0m\033[1;33mref_r->pc = 0x%x\n\033[0m",ref_r->pc);
-    printf("\033[1;31m//\033[0m\033[1;35m cpu ->pc = 0x%x\n\033[0m",cpu.pc);
-    printf("\033[1;31m////////////////////////\n\033[0m");
+    printf("\033[1;31m--------!Difftest!--------\n\033[0m");
+    printf("\033[1;31m//////////////////////////\n\033[0m");   
+    printf("\033[1;31m//\033[0m\033[1;33mref_r->pc = 0x%x\033[1;31m//\033[0m\n",ref_r->pc);
+    printf("\033[1;31m//\033[0m\033[1;35m cpu ->pc = 0x%x\033[1;31m//\033[0m\n",cpu.pc);
+    printf("\033[1;31m//////////////////////////\n\033[0m");
     return false;
   }
   return true;
