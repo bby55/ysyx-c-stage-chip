ccfe8d5be220ae1537b3c910e21d3cfacb8d25a9
tracer-ysyx <tracer@ysyx.org>
2025-07-28 10:39:23 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:39:23 up  1:26,  2 users,  load average: 0.72, 0.88, 0.54

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -92,6 +92,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 }
 
 static void checkregs(CPU_state *ref, vaddr_t pc) {
+  printf("!!!!!!:%d\n",isa_difftest_checkregs(ref, pc));
   if (isa_difftest_checkregs(ref, pc) == false) {
     nemu_state.state = NEMU_ABORT;
     nemu_state.halt_pc = pc;
