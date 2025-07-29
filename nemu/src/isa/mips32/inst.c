8931d557d8757b0556dd91ae5384668894a9578d
tracer-ysyx <tracer@ysyx.org>
2025-07-29 10:32:29 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:32:29 up  1:17,  2 users,  load average: 0.46, 0.42, 0.33

--- nemu/src/isa/mips32/inst.c
+++ nemu/src/isa/mips32/inst.c
@@ -73,5 +73,6 @@ static int decode_exec(Decode *s) {
 int isa_exec_once(Decode *s) {
   s->isa.inst = inst_fetch(&s->snpc, 4);
   IFDEF(CONFIG_ITRACE, iringbuf_write(s->pc, s->isa.inst.val));
+  printf("%x",iringbuf.pc);
   return decode_exec(s);
 }
