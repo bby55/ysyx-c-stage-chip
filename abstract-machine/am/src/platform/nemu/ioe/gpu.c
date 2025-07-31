f144de90d1a1c099583e765718ed209d93bb59cc
tracer-ysyx <tracer@ysyx.org>
2025-07-31 16:22:25 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:22:25 up  5:42,  2 users,  load average: 0.24, 0.29, 0.34

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -9,26 +9,26 @@ void __am_gpu_init() {
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   uint32_t wh_data = inl(VGACTL_ADDR);
-  uint32_t width = wh_data & 0xFFFF;
-  uint32_t height = wh_data >> 16;
-  //uint32_t vmemsz = width*height*4;
-
+  uint32_t h = wh_data & 0xffff;
+  uint32_t w = wh_data >> 16;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = width, .height = height,
+    .width = w, .height = h,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-  if (!ctl->sync && (w == 0 || h == 0)) return;
+  uint32_t screen_wid = inl(VGACTL_ADDR) >> 16;
+  int x = ctl->x;
+  int y = ctl->y;
+  int w = ctl->w;
+  int h = ctl->h;
   uint32_t *pixels = ctl->pixels;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
   for (int i = y; i < y+h; i++) {
     for (int j = x; j < x+w; j++) {
-      fb[screen_w*i+j] = pixels[w*(i-y)+(j-x)];
+      fb[screen_wid*i+j] = pixels[w*(i-y)+(j-x)];
     }
   }
   if (ctl->sync) {
