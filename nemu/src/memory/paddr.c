da1b9ee59143f5362fe1666208627dbc2cf8d4cf
tracer-ysyx <tracer@ysyx.org>
2025-08-03 20:20:31 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:20:31 up 11:11,  2 users,  load average: 0.34, 0.49, 0.36

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -12,20 +12,21 @@
 *
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
-
 #include <memory/host.h>
 #include <memory/paddr.h>
 #include <device/mmio.h>
 #include <isa.h>
 
-#if   defined(CONFIG_PMEM_MALLOC)
-static uint8_t *pmem = NULL;
+#if defined(CONFIG_PMEM_MALLOC)
+uint8_t *pmem = NULL;
 #else // CONFIG_PMEM_GARRAY
-static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
+uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
 
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
+
+// (其余代码保持不变)
 
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
