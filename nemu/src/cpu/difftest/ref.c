2bc29fdb59a6f64cfca19dce3e2537dd30e2247e
tracer-ysyx <tracer@ysyx.org>
2025-08-03 19:31:44 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:31:44 up 10:22,  2 users,  load average: 0.77, 0.27, 0.13

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -1,42 +1,49 @@
-/***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
-
 #include <isa.h>
 #include <cpu/cpu.h>
-#include <difftest-def.h>
+#include <cpu/decode.h>
+#include <cpu/ifetch.h>
 #include <memory/paddr.h>
+#include <difftest-def.h>
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+  if (direction == DIFFTEST_TO_REF) {
+    for (size_t i = 0; i < n; i++) {
+      paddr_write(addr + i, 1, ((uint8_t *)buf)[i]);
+    }
+  } else {
+    for (size_t i = 0; i < n; i++) {
+      ((uint8_t *)buf)[i] = paddr_read(addr + i, 1);
+    }
+  }
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+  CPU_state *npc_cpu = (CPU_state *)dut;
+  if (direction == DIFFTEST_TO_REF) {
+    memcpy(&cpu.gpr, &npc_cpu->gpr, sizeof(cpu.gpr));
+    cpu.pc = npc_cpu->pc;
+  } else {
+    memcpy(&npc_cpu->gpr, &cpu.gpr, sizeof(cpu.gpr));
+    npc_cpu->pc = cpu.pc;
+  }
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
-  assert(0);
+  Decode s;
+  for (uint64_t i = 0; i < n; i++) {
+    s.pc = cpu.pc;
+    s.snpc = s.pc + 4;
+    isa_exec_once(&s);
+    cpu.pc = s.dnpc;
+  }
 }
 
 __EXPORT void difftest_raise_intr(word_t NO) {
   assert(0);
 }
-
-__EXPORT void difftest_init(int port) {
   void init_mem();
+  
+__EXPORT void difftest_init(int port) {
   init_mem();
-  /* Perform ISA dependent initialization. */
   init_isa();
 }
