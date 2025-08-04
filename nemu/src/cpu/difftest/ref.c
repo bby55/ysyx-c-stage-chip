929d3dc15de4b4313342c136131365e3918d1c0a
tracer-ysyx <tracer@ysyx.org>
2025-08-04 12:00:47 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:00:47 up  2:54,  2 users,  load average: 0.50, 0.39, 0.27

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -30,7 +30,13 @@ __EXPORT void difftest_regcpy(void *dut, bool direction) {
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
+    static int skip_count = 1; // 延缓两个周期
     Decode s;
+    if (skip_count > 0) {
+        Log("difftest_exec: skipping cycle %d, cpu.pc = " FMT_WORD, skip_count, cpu.pc);
+        skip_count--;
+        return;
+    }
     for (uint64_t i = 0; i < n; i++) {
         s.pc = cpu.pc;
         s.snpc = s.pc + 4;
