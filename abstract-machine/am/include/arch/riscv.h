dc68e058a8032c35e285b904524eb83029431907
tracer-ysyx <tracer@ysyx.org>
2025-08-05 19:06:59 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:06:59 up 10:00,  2 users,  load average: 0.24, 0.33, 0.34

--- abstract-machine/am/include/arch/riscv.h
+++ abstract-machine/am/include/arch/riscv.h
@@ -7,10 +7,13 @@
 #define NR_REGS 32
 #endif
 
+
 struct Context {
   // TODO: fix the order of these members to match trap.S
-  uintptr_t mepc, mcause, gpr[NR_REGS], mstatus;
+  uintptr_t gpr[32];
+  uintptr_t mcause, mstatus, mepc;
   void *pdir;
+  uintptr_t np;
 };
 
 #ifdef __riscv_e
