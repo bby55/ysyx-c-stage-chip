cd902cfd7874158c6e59cd8cf9def183cbd6383c
tracer-ysyx <tracer@ysyx.org>
2025-08-07 10:33:17 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:33:17 up  1:28,  2 users,  load average: 0.26, 0.68, 0.52

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -48,16 +48,16 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
         return false;
     }
     if (ref_r->csr.mtvec != cpu.csr.mcause) {
-        //printf("mcause is different! ref: 0x%08x, current: 0x%08x\n", ref_r->csr.mcause, cpu.csr.mcause);
-        //return false;
+        printf("mcause is different! ref: 0x%08x, current: 0x%08x\n", ref_r->csr.mcause, cpu.csr.mcause);
+        return false;
     }
     if (ref_r->csr.mepc != cpu.csr.mepc) {
         printf("mepc is different! ref: 0x%08x, current: 0x%08x\n", ref_r->csr.mepc, cpu.csr.mepc);
         return false;
     }
     if (ref_r->csr.mcause != cpu.csr.mtvec) {
-        //printf("mtvec is different! ref: 0x%08x, current: 0x%08x\n", ref_r->csr.mtvec, cpu.csr.mtvec);
-        //return false;
+        printf("mtvec is different! ref: 0x%08x, current: 0x%08x\n", ref_r->csr.mtvec, cpu.csr.mtvec);
+        return false;
     }
     return true;
 }
