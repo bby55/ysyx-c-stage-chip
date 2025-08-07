92fe4216bb6914c0c0475e288f081f5141674d56
tracer-ysyx <tracer@ysyx.org>
2025-08-07 10:16:37 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:16:37 up  1:11,  2 users,  load average: 0.22, 0.26, 0.31

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -45,11 +45,11 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 
   if (ref_r->csr.mstatus != cpu.csr.mstatus) {
         printf("mstatus is different! ref: 0x%08x, current: 0x%08x\n", ref_r->csr.mstatus, cpu.csr.mstatus);
-        //return false;
+        return false;
     }
     if (ref_r->csr.mcause != cpu.csr.mcause) {
         printf("mcause is different! ref: 0x%08x, current: 0x%08x\n", ref_r->csr.mcause, cpu.csr.mcause);
-        return false;
+        //return false;
     }
     if (ref_r->csr.mepc != cpu.csr.mepc) {
         printf("mepc is different! ref: 0x%08x, current: 0x%08x\n", ref_r->csr.mepc, cpu.csr.mepc);
