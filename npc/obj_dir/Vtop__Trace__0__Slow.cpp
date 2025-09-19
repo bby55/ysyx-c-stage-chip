b2a72c06554fc14bd8803d97aa672986619b5c5c
tracer-ysyx <tracer@ysyx.org>
2025-09-19 21:21:08 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:21:08 up  7:46,  2 users,  load average: 0.81, 0.74, 0.84

--- npc/obj_dir/Vtop__Trace__0__Slow.cpp
+++ npc/obj_dir/Vtop__Trace__0__Slow.cpp
@@ -1009,7 +1009,7 @@ VL_ATTR_COLD void Vtop___024root__trace_full_sub_0(Vtop___024root* vlSelf, Veril
     bufp->fullIData(oldp+436,(vlSelf->top__DOT__u_regfile__DOT__rf[30]),32);
     bufp->fullIData(oldp+437,(vlSelf->top__DOT__u_regfile__DOT__rf[31]),32);
     bufp->fullIData(oldp+438,(((0U == vlSelf->top__DOT__u_regfile__DOT__rf
-                                [0xfU]) ? ((IData)(4U) 
+                                [0xaU]) ? ((IData)(4U) 
                                            + vlSelf->top__DOT__pc)
                                 : vlSelf->top__DOT__pc)),32);
     bufp->fullBit(oldp+439,(vlSelf->clk));
