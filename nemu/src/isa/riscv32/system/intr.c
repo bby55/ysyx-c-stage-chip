c3ed91f3b2a6ce18501a88e79903e0f7b8ff0621
tracer-ysyx <tracer@ysyx.org>
2025-09-19 11:48:21 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:48:21 up 19:47,  2 users,  load average: 1.37, 1.11, 0.67

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -17,15 +17,12 @@
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 
-  cpu.csr.mstatus &= ~(1<<7);
-  cpu.csr.mstatus |= ((cpu.csr.mstatus&(1<<3))<<4);
-  cpu.csr.mstatus &= ~(1<<3);
-  cpu.csr.mstatus |= ((1<<11)+(1<<12));
+  // cpu.csr.mstatus &= ~(1<<7);
+  // cpu.csr.mstatus |= ((cpu.csr.mstatus&(1<<3))<<4);
+  // cpu.csr.mstatus &= ~(1<<3);
+  // cpu.csr.mstatus |= ((1<<11)+(1<<12));
 
-  // if(NO==0){
-  //   epc+=4;
-  //   printf("chenpang91\n");
-  // }
+  
   
   cpu.csr.mcause = NO;
   cpu.csr.mepc = epc;
