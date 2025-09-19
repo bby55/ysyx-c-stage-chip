8d3101fd0db64d968ef6a5195a61145685335458
tracer-ysyx <tracer@ysyx.org>
2025-09-19 21:15:23 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:15:23 up  7:40,  2 users,  load average: 0.16, 0.65, 0.86

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -9,9 +9,7 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
      //printf("mcause = 0x%d\n", (unsigned long)c->mcause);
     switch (c->mcause) {
-      //case 0: ev.event = EVENT_SYSCALL; break;
-      case -1: ev.event = EVENT_YIELD; break;
-      case 11: ev.event = EVENT_YIELD; break;
+      case 11: ev.event = EVENT_YIELD; c->mepc += 4; break;
       default: ev.event = EVENT_ERROR; break;
     }
 
@@ -36,7 +34,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *kctx = (Context *)(kstack.end-sizeof(Context));
-  kctx->mepc=(uintptr_t) entry - 4;
+  kctx->mepc=(uintptr_t) entry;
   kctx->gpr[10] = (uintptr_t)arg;
   return kctx;
 }
