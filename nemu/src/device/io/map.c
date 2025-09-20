3d965b9cab1c6d13a91711abbe9986ad3fe09926
tracer-ysyx <tracer@ysyx.org>
2025-09-21 07:38:09 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  07:38:09 up 13:34,  2 users,  load average: 0.45, 0.69, 0.49

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -59,9 +59,9 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
 
-  #ifdef CONFIG_DTARCE_COND
+  // #ifdef CONFIG_DTARCE_COND
    log_write("dtrace: read %10s at " FMT_PADDR ",%d\n", map->name, addr, len);
- #endif
+//  #endif
 
   return ret;
 }
