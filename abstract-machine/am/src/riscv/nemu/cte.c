f1257d0a9fb305dc3ca0ce4f78c167afd40f28a6
tracer-ysyx <tracer@ysyx.org>
2025-09-21 00:36:32 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:36:32 up  6:32,  2 users,  load average: 0.26, 0.78, 0.81

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -3,33 +3,17 @@
 #include <klib.h>
 
 static Context* (*user_handler)(Event, Context*) = NULL;
-#define CONFIG_ETRACE
 
 Context* __am_irq_handle(Context *c) {
-
   if (user_handler) {
     Event ev = {0};
-
-    uint32_t cause = c->mcause;
-
-    switch (cause) {
-      case 8: case 9: case 11:
-        ev.event=EVENT_YIELD;
-          c->mepc += 4;
-          #ifdef CONFIG_ETRACE
-          // printf("Trap: EVENT_YIELD\n"); 
-          #endif
-        break;
-      default: ev.event = EVENT_ERROR; 
-          #ifdef CONFIG_ETRACE
-          printf("Trap: EVENT_ERROR\n"); 
-          #endif
-        break;
+    switch (c->mcause) {
+      case 11: ev.event = EVENT_YIELD; c->mepc += 4;break;
+      default: ev.event = EVENT_ERROR; break;
     }
-    // printf("处理前上下文: %d\n", c);
+
     c = user_handler(ev, c);
     assert(c != NULL);
-    // printf("处理后上下文: %d\n", c);
   }
 
   return c;
@@ -47,29 +31,20 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
   return true;
 }
 
-// #define CONTEXT_SIZE  ((NR_REGS + 3) * 4)
-
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  uintptr_t stack_top = (uintptr_t)(kstack.end);
-  stack_top = stack_top & ~0xF;
-  Context *c = (Context*)(stack_top - sizeof(Context));
-  memset(c, 0, sizeof(Context));
-  c->mepc = (uintptr_t)entry;
-  c->gpr[10] = (uintptr_t)arg;
-  c->gpr[2] = stack_top;
-  c->mstatus = 0x1800;
-  // printf("创建上下文: %d -> 大小=%d, 栈顶=%d\n", 
-  //      c, sizeof(Context), stack_top);
-  return c;
+  Context *kctx = (Context *)(kstack.end-sizeof(Context));
+  kctx->mepc=(uintptr_t) entry;
+  kctx->mstatus=0x1800;
+  //printf("cp=%d\n",kctx->mepc);
+  kctx->gpr[10] = (uintptr_t)arg;
+  return kctx;
 }
 
 void yield() {
-
-  // printf("Sizeof Context: %d bytes\n", sizeof(Context));
 #ifdef __riscv_e
   asm volatile("li a5, -1; ecall");
 #else
-  asm volatile("li a7, -1; ecall");
+  asm volatile("li a7, 11; ecall");
 #endif
 }
 
