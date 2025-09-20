8ea60ff73e8df8d5e6e3fb21905f88c7bf889a21
tracer-ysyx <tracer@ysyx.org>
2025-09-21 03:14:21 +0800

    > compile NEMU ysyx_22040000 白冰洋 Linux ysyxbby-VMware-Virtual-Platform 6.11.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 13 20:29:41 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  03:14:21 up  9:10,  2 users,  load average: 1.50, 0.65, 0.64

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -57,11 +57,13 @@ static void init_screen() {
 }
 
 static inline void update_screen() {
+  printf("noam\n");
   SDL_UpdateTexture(texture, NULL, vmem, SCREEN_W * sizeof(uint32_t));
   SDL_RenderClear(renderer);
   SDL_RenderCopy(renderer, texture, NULL, NULL);
   SDL_RenderPresent(renderer);
 }
+
 #else
 static void init_screen() {}
 printf("typinggame\n");
